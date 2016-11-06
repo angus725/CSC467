59494dffda04efb11003406761192cdca233ebc8
Angus <angus725@gmail.com>
2016-11-06 17:26:30 -0500

    added/renamed test cases

--- a2/Starter2/testParser.sh
+++ a2/Starter2/testParser.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo Running parse_test_positve
 echo ----------------------------------------------------------
-./compiler467 -Tp ./parserTests/parse_test_positve.frag
+./compiler467 -Tp ./cases/parse_test_positve.frag
 
 echo ----------------------------------------------------------
 echo Running parse_test_negative
 echo ----------------------------------------------------------
-./compiler467 -Tp ./parserTests/parse_test_negative.frag 
+./compiler467 -Tp ./cases/parse_test_negative.frag 
 echo ----------------------------------------------------------
 
 echo done
